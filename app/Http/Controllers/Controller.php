df9109f957f05e6d07c066a74d2246bea7914eaa
Dobos <djpolmd@gmail.com>
2018-08-23 09:04:08 +0000

    futher land

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace app\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+}
+
+class UserController extends Controller
+{
+    /**
+     * Show the profile for the given user.
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        return view('user.profile', ['user' => User::findOrFail($id)]);
+    }
+}
+    
+
---
+++ app/routes.php
@@ -0,0 +1,8 @@
+<php?
+
+Route::get('users', function()
+{
+    return 'Users!';
+});
+
+?>
