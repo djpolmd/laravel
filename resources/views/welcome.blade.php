2c65892a1d1a2f3af6e203869824cb9d107dc66a
Dobos Pavel <djpolmd@gmail.com>
2018-09-09 17:35:33 +0300

    dnjh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,97 +1,99 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Articles test page</title>
+		<title>Articles test page</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+		<!-- Fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+		<!-- Styles -->
+		<style>
+			html, body {
+				background-color: #fff;
+				color: #636b6f;
+				font-family: 'Nunito', sans-serif;
+				font-weight: 200;
+				height: 100vh;
+				margin: 0;
+			}
 
-            .full-height {
-                height: 100vh;
-            }
+			.full-height {
+				height: 100vh;
+			}
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+			.flex-center {
+				align-items: center;
+				display: flex;
+				justify-content: center;
+			}
 
-            .position-ref {
-                position: relative;
-            }
+			.position-ref {
+				position: relative;
+			}
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+			.top-right {
+				position: absolute;
+				right: 10px;
+				top: 18px;
+			}
 
-            .content {
-                text-align: center;
-            }
+			.content {
+				text-align: center;
+			}
 
-            .title {
-                font-size: 84px;
-            }
+			.title {
+				font-size: 84px;
+			}
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+			.links > a {
+				color: #636b6f;
+				padding: 0 25px;
+				font-size: 12px;
+				font-weight: 600;
+				letter-spacing: .1rem;
+				text-decoration: none;
+				text-transform: uppercase;
+			}
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
+			.m-b-md {
+				margin-bottom: 30px;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="flex-center position-ref full-height">
+			@if (Route::has('login'))
+				<div class="top-right links">
+					@auth
+						<a href="{{ url('/home') }}">Home</a>
+					@else
+						<a href="{{ route('login') }}">Login</a>
+						<a href="{{ route('register') }}">Register</a>
+					@endauth
+				</div>
+			@endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Articles test page
-                </div>
+			<div class="content">
+				<div class="title m-b-md">
+					Articles test page
+				</div>
 
-                <div class="links">
-                    
-                    <a href="https://laravel-djpolmd.c9users.io/articles/add">Adaugare articol</a>
-                    <a href="https://laravel-djpolmd.c9users.io/articles/edit">Editare articol</a>
-                   
-                    <a href="https://laravel-djpolmd.c9users.io/articles/update/">Modificare articol</a>
-                    <a href="https://laravel-djpolmd.c9users.io/articles">Vizualiare articole (lista)</a>
-                    <a href="https://laravel-djpolmd.c9users.io/articles/id_search">Vizualizare articol cu toate detaliile articolului</a>
-                </div>
-            </div>
-        </div>
-    </body>
+				<div class="links">
+					
+					<a href="/add">Adaugare articol</a>
+					<a href="/edit">Editare articol</a>
+				   
+					<a href="/update/">Modificare articol</a>
+					<a href="/articles">Vizualiare articole (lista)</a>
+					<a href="/articles/id_search">Vizualizare articol cu toate detaliile articolului</a>
+				</div>
+				<div> ... </div>
+				<div class="links"> <a href="/users">Lista  de utilizatori -> </a> </div>
+			</div>
+		</div>
+	</body>
 </html>
